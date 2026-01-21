d3669b27b0100168045eea892cfb61662f4bd883
Brian Smith <bsmith@axim.org>
2026-01-21 16:32:12 -0500

    chore: messy test runtime config

--- runtime/initialize.js
+++ runtime/initialize.js
@@ -90,6 +90,7 @@ import {
 } from './logging';
 import { GoogleAnalyticsLoader } from './scripts';
 import { publish } from './subscriptions';
+import { EnvironmentTypes } from '../types';
 
 /**
  * If set in configuration, a basename will be prepended to all relative routes under BrowserRouter.
@@ -165,17 +166,19 @@ async function fileConfig() {
  */
 async function runtimeConfig() {
   try {
-    const { mfeConfigApiUrl, siteId } = getSiteConfig();
+    const { runtimeConfigJsonUrl, environment } = getSiteConfig();
+    debugger;
 
-    if (mfeConfigApiUrl) {
+    if (runtimeConfigJsonUrl) {
       const apiConfig = { headers: { accept: 'application/json' } };
       const apiService = await configureCache();
 
-      const params = new URLSearchParams();
-      params.append('mfe', siteId);
-      const url = `${mfeConfigApiUrl}?${params.toString()}`;
+      const runtimeConfigUrl = new URL(runtimeConfigJsonUrl);
+      if (environment === EnvironmentTypes.DEVELOPMENT) {
+        runtimeConfigUrl.searchParams.set('timestamp', Date.now());
+      }
 
-      const { data } = await apiService.get(url, apiConfig);
+      const { data } = await apiService.get(runtimeConfigUrl.toString(), apiConfig);
       mergeSiteConfig(data);
     }
   } catch (error) {
